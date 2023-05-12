cb170103f6779701d8a7f906bbd81a3593c4b2a2
jng <junhanngd1115@gmail.com>
2023-05-12 12:19:04 +0800

    helloe

--- git/setup_precommit.sh
+++ git/setup_precommit.sh
@@ -11,9 +11,9 @@ cat > "$REPO_PATH_GIT/hooks/commit-msg" << "EOF"
 
 MSG="$(cat $1)"
 
-if [ $MSG == "feat:*" ] || [ $MSG == "chore:*" ] || [ $MSG == "ci:*" ] || [ $MSG == "chore:*" ] || [ $MSG == "docs:*" ] \
+if ![[ $MSG == "feat:*" ] || [ $MSG == "chore:*" ] || [ $MSG == "ci:*" ] || [ $MSG == "chore:*" ] || [ $MSG == "docs:*" ] \
 || [ $MSG == "fix:*" ] || [ $MSG == "perf:*" ] || [ $MSG == "refactor:*" ] || [ $MSG == "revert:*" ] || [ $MSG == "style:*" ]|| [ $MSG == "test:*" ] \
- ;
+] ;
 	then
 		echo "Your commit message type must be one of [build, chore, ci, docs, feat, fix, perf, refactor, revert, style, test]"
 		echo "Get help: https://github.com/conventional-changelog/commitlint/#what-is-commitlint" 
