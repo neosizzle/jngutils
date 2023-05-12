f90bcc547987f163d3ad399828c35dd190092f3c
jng <junhanngd1115@gmail.com>
2023-05-12 12:41:48 +0800

    feat: precommit

--- git/setup_precommit.sh
+++ git/setup_precommit.sh
@@ -11,13 +11,12 @@ cat > "$REPO_PATH_GIT/hooks/commit-msg" << "EOF"
 
 MSG="$(cat $1)"
 
-if [ "$MSG" != "feat:*" ] && [ "$MSG" != "chore:*" ] && [ "$MSG" != "ci:*" ] && [ "$MSG" != "chore:*" ] && [ "$MSG" != "docs:*" ] \
-&& [ "$MSG" != "fix:*" ] && [ "$MSG" != "perf:*" ] && [ "$MSG" != "refactor:*" ] && [ "$MSG" != "revert:*" ] && [ "$MSG" != "style:*" ] && [ "$MSG" == "test:*" ] \
- ;
-	then
-		echo "Your commit message type must be one of [build, chore, ci, docs, feat, fix, perf, refactor, revert, style, test]"
-		echo "Get help: https://github.com/conventional-changelog/commitlint/#what-is-commitlint" 
-		exit 1
+if [[ $MSG != "feat: "* ]] &&  [[ $MSG != "chore: "* ]] && [[ $MSG != "ci: "* ]] && [[ $MSG != "chore: "* ]] && [[ $MSG != "docs: "* ]] && [[ $MSG != "fix: "* ]] && [[ $MSG != "perf: "* ]] &&
+[[ $MSG != "refactor: "* ]] && [[ $MSG != "revert: "* ]] && [[ $MSG != "style: "* ]] && [[ $MSG != "test: "* ]] ;
+        then
+                echo "Your commit message type must be one of [build, chore, ci, docs, feat, fix, perf, refactor, revert, style, test]"
+                echo "Get help: https://github.com/conventional-changelog/commitlint/#what-is-commitlint" 
+                exit 1
 fi
 EOF
 
--- test.sh
+++ test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # created by jngutils
 
-MSG="chore: 123"
-#  [ $MSG != "ci:*" ] && [ $MSG != "chore:*" ] && [ $MSG != "docs:*" ] \
-# && [ $MSG != "fix:*" ] && [ $MSG != "perf:*" ] && [ $MSG != "refactor:*" ] && [ $MSG != "revert:*" ] && [ $MSG != "style:*" ] && [ $MSG == "test:*" ] \
-
-if [ $MSG != "feat:*" ] && [ $MSG != "chore:*" ] ;
+MSG="fisadsad123"
+#  [[ $MSG != "ci: "* ]] && [[ $MSG != "chore: "* ]] && [[ $MSG != "docs: "* ]] \
+# && [[ $MSG != "fix: "* ]] && [[ $MSG != "perf: "* ]] && [[ $MSG != "refactor: "* ]] && [[ $MSG != "revert: "* ]] && [[ $MSG != "style: "* ]] && [[ $MSG == "test: "* ]] \
+echo $MSG
+if [[ $MSG != "feat: "* ]] &&  [[ $MSG != "chore: "* ]] && [[ $MSG != "ci: "* ]] && [[ $MSG != "chore: "* ]] && [[ $MSG != "docs: "* ]] && [[ $MSG != "fix: "* ]] && [[ $MSG != "perf: "* ]] &&
+[[ $MSG != "refactor: "* ]] && [[ $MSG != "revert: "* ]] && [[ $MSG != "style: "* ]] && [[ $MSG != "test: "* ]] ;
         then
                 echo "Your commit message type must be one of [build, chore, ci, docs, feat, fix, perf, refactor, revert, style, test]"
                 echo "Get help: https://github.com/conventional-changelog/commitlint/#what-is-commitlint" 
                 exit 1
 fi
+
+echo $MSG
